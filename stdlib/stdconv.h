08e041565258f0e93a51f19f60ea4df995311c58
Collin Reeser <collin.reeser@gmail.com>
2015-07-27 23:20:21 -0400

    Resolves Issue #36

--- stdlib/stdconv.h
+++ stdlib/stdconv.h
@@ -1,7 +1,15 @@
 #ifndef STDCONV_H
 #define STDCONV_H
 
+struct MaybeChar
+{
+    uint32_t refCount;
+    uint32_t variantTag;
+    char c;
+};
+
 int ord(char c);
+void* chr(int c);
 void* charToString(char c);
 
 #endif
